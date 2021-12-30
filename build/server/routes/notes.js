f5aea5e8bc5467cba9ce7f34ad44b5f0af18871f
stufade <stufade223@gmail.com>
2021-12-30 14:18:40 +0300

    Refactor code

--- build/server/routes/notes.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=notes.js.map
--- build/server/server/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.__port__ = exports.__prod__ = void 0;
-exports.__prod__ = process.env.NODE_ENV == "prod";
-exports.__port__ = process.env.PORT || 3001;
